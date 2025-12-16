cac2339e4d6aa63d75c5981882707c1ddb20a0e2
kiokuless <me+kiokuless@tokinia.me>
2025-12-16 20:07:00 +0900

    WIP but good

--- main.tf
+++ main.tf
@@ -21,3 +21,20 @@ resource "google_artifact_registry_repository" "my_registry" {
     repository_id = "registry-id-1"
     format = "DOCKER"
 }
+
+resource "google_cloud_run_v2_service" "my_service" {
+  name = "oidc-server"
+  location = "asia-northeast1"
+  deletion_protection = false
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  scaling {
+    max_instance_count = 2
+  }
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+  }
+}
