7a695f775e48da5ce452fce58ca4157261afa640
kiokuless <me+kiokuless@tokinia.me>
2025-12-16 19:55:25 +0900

    apply した

--- main.tf
+++ main.tf
@@ -5,3 +5,19 @@ terraform {
        }
     }
 }
+
+variable "project_id" {
+  description = "GCPプロジェクトID"
+  type        = string
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "asia-northeast1"
+}
+
+resource "google_artifact_registry_repository" "my_registry" {
+    location = "asia-northeast1"
+    repository_id = "registry-id-1"
+    format = "DOCKER"
+}
